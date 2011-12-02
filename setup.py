8c0b10ba1d6b07256d5d21df1739636ea57bd5f2
Jay Wineinger <jay.wineinger@gmail.com>
2011-12-02 00:22:34 -0600

    Add packaging scripts

---
+++ setup.py
@@ -0,0 +1,32 @@
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+setup(
+    name = "django-exportable-admin",
+    version = "2.0",
+
+    packages = find_packages(),
+
+    author = "Jay Wineinger",
+    author_email = "jay.wineinger@gmail.com",
+
+    description = "An app to allow Django admin changelists to be exported to CSV",
+    long_description=open('README.txt').read(),
+
+    url = "https://github.com/jwineinger/django-exportable-admin",
+    download_url = "https://github.com/jwineinger/django-exportable-admin/downloads",
+
+    install_requires = [
+        "Django >= 1.4"
+    ],
+
+    include_package_data = True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
