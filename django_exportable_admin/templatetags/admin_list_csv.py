8ea63eeffbacd207b7256acc1b525b5b7283bf22
Jay Wineinger <jay.wineinger@gmail.com>
2011-12-05 20:56:44 -0600

    Fix result_list templatetag so that export_delimiter is available in the results template

--- django_exportable_admin/templatetags/admin_list_csv.py
+++ django_exportable_admin/templatetags/admin_list_csv.py
@@ -4,12 +4,15 @@ from django.contrib.admin.templatetags.admin_list import result_headers, results
 register = Library()
 
 
-@register.inclusion_tag("django_exportable_admin/change_list_results_csv.html")
-def result_list(cl):
+@register.inclusion_tag("django_exportable_admin/change_list_results_csv.html",
+                        takes_context=True)
+def result_list(context):
     """
     Displays the headers and data list together
     """
-    headers = list(result_headers(cl))
+    cl = context['cl']
     return {'cl': cl,
-            'result_headers': headers,
-            'results': list(results(cl))}
+            'result_headers': list(result_headers(cl)),
+            'results': list(results(cl)),
+            'export_delimiter' : context['export_delimiter'],
+           }
